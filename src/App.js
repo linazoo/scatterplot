0855e95b60f2ac8b36586ab6087d43893ad1d720
linazoo <linazooper@gmail.com>
2018-03-12 15:47:22 -0500

    added sample data with birthds and deaths - loaded data via componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import {csv} from 'd3-request';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    csv('./src/data/birthdeathrates.csv', (error, data) => {
+      if (error) {
+        this.setState({loadError: true});
+      }
+      this.setState({
+        data: data.map(d => ({...d, x: Number(d.birth), y: Number(d.death)}))
+      });
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
