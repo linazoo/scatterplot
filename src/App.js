bc5d557cbb1fdc7a6dd38ee23e63010e5de5c7f3
linazoo <linazooper@gmail.com>
2018-03-12 15:53:44 -0500

    updated render method to show the Scatterplot component if there is data

--- src/App.js
+++ src/App.js
@@ -23,18 +23,26 @@ class App extends Component {
 
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    if (this.state.loadError) {
+      return <div>couldn't load file</div>;
+    }
+    if (!this.state.data) {
+      return <div />;
+    }
+    return <div style={{
+      background: '#fff',
+      borderRadius: '3px',
+      boxShadow: '0 1 2 0 rgba(0,0,0,0.1)',
+      margin: 12,
+      padding: 24,
+      width: '350px'
+    }}>
+      <h1>Birth and death rates of selected countries</h1>
+      <h2>per 1,000 inhabitants</h2>
+      <Scatterplot data={this.state.data}/>
+    </div>;
   }
 }
+
 
 export default App;
